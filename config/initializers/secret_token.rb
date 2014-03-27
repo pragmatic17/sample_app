aac5876f380b70dc609d57df81b9d6a792a9a58c
Chris G <thegoingoforit@yahoo.com>
2014-03-26 22:45:53 -0600

    money is as money does

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'ab4428fc4893f85367608acd8c35c1d7636baa6d65cb652a7fd9443d5b74d759e9f3ec3e0aca92d4d01709d340b706468d7a2290d6fc555a0930d1558e89e6b8'
